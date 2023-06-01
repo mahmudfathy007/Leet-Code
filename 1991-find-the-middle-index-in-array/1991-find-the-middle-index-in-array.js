df6783667c709ab5d5ac974eb65460f9143bfd88
Mahmoud fathy <mahmudfathy007@gmail.com>
2023-06-02 01:53:31 +0300

    Time: 61 ms (67.96%), Space: 42.5 MB (39.08%) - LeetHub

---
+++ 1991-find-the-middle-index-in-array/1991-find-the-middle-index-in-array.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMiddleIndex = function(nums) {
+    
+    var sum = 0;    
+    var leftSum = 0;
+    
+    for(var i = 0 ; i < nums.length ; i++){
+        sum += nums[i];
+    }
+    
+    for(var i = 0 ; i < nums.length ; i++){
+        if(leftSum === sum - leftSum - nums[i]){
+            return i;
+        }
+        leftSum += nums[i];
+    }
+    
+    return -1;
+};
