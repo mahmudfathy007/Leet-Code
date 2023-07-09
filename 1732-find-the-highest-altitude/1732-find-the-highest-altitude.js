97d68193d2dac176089aabff34119cd2beb66fb2
Mahmoud fathy <mahmudfathy007@gmail.com>
2023-07-10 02:31:18 +0300

    Time: 56 ms (69.05%), Space: 42.4 MB (7.96%) - LeetHub

---
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.js
@@ -0,0 +1,11 @@
+var largestAltitude = function(gain) {
+  let maxAltitude = 0;
+  let currentAltitude = 0;
+
+  for (let i = 0; i < gain.length; i++) {
+    currentAltitude += gain[i];
+    maxAltitude = Math.max(maxAltitude, currentAltitude);
+  }
+
+  return maxAltitude;
+};
