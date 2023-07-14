be95cd7d087a5d549bf45e43c3c67b265d5cec50
Mahmoud fathy <mahmudfathy007@gmail.com>
2023-07-14 10:18:56 +0300

    Time: 55 ms (73.80%), Space: 41.5 MB (73.80%) - LeetHub

--- 1025-divisor-game/1025-divisor-game.js
+++ 1025-divisor-game/1025-divisor-game.js
@@ -1,12 +1,3 @@
-/**
- * @param {number} n
- * @return {boolean}
- */
 var divisorGame = function(n) {
-    if(n % 2 == 0){
-      return true;
-    }
-    else{
-      return false;
-    }
+  return n % 2 === 0;
 };
